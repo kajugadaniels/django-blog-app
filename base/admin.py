3452c6e9d75e510274f41e18fc94354263376769
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-22 14:13:18 +0200

    Register Category model in admin with custom list display and action links

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,22 @@
+from django import forms
+from base.models import *
+from django.urls import reverse
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at', 'updated_at', 'edit_link', 'delete_link')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at', 'updated_at')
+    list_per_page = 20
+
+    def edit_link(self, obj):
+        url = reverse("admin:home_category_change", args=[obj.pk])
+        return format_html('<a class="button" href="{}">Edit</a>', url)
+    edit_link.short_description = "Edit"
+    
+    def delete_link(self, obj):
+        url = reverse("admin:home_category_delete", args=[obj.pk])
+        return format_html('<a class="button" href="{}">Delete</a>', url)
+    delete_link.short_description = "Delete"
