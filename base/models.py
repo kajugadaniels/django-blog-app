bdc2fb2699b1f343987dbb290174cc4efc065c1f
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-24 09:11:48 +0200

    Add ArticleImage model with image preview feature

--- base/models.py
+++ base/models.py
@@ -5,8 +5,8 @@ from django.db import models
 from django.db.models import Sum
 from django.utils import timezone
 from django.utils.text import slugify
+from django.utils.html import format_html
 from django.contrib.auth.models import User
-from taggit.managers import TaggableManager
 from imagekit.processors import ResizeToFill
 from imagekit.models import ProcessedImageField
 from django.core.exceptions import ValidationError
@@ -104,3 +104,21 @@ class Article(models.Model):
 
     class Meta:
         verbose_name_plural = "Articles"
+
+class ArticleImage(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='article_images/')
+    caption = models.CharField(max_length=255, null=True, blank=True)
+
+    def image_preview(self):
+        """Display a thumbnail of the article image in the list view."""
+        if self.image:
+            return format_html('<img src="{}" width="50" height="50" />', self.image.url)
+        return "No image"
+    image_preview.short_description = "Image Preview"
+
+    def __str__(self):
+        return self.caption if self.caption else "No Caption"
+
+    class Meta:
+        verbose_name_plural = "Article Images"
