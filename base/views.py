0c54bba26782b55733de52641473fc9bd0f57987
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-25 11:11:04 +0200

    Update home view: retrieve topArticle (most views in last 24h), add humanized timestamps & article image preview, and use camelCase naming

--- base/views.py
+++ base/views.py
@@ -1,11 +1,19 @@
 from base.models import *
+from datetime import timedelta
+from django.utils import timezone
 from django.shortcuts import render
 
 def home(request):
     breakingNews = Article.objects.all().order_by('-id')[:4]
-
+    
+    # Define the time threshold for 24 hours ago
+    timeThreshold = timezone.now() - timedelta(hours=24)
+    # Retrieve the article with the most views in the last 24 hours
+    topArticle = Article.objects.filter(created_at__gte=timeThreshold).order_by('-views').first()
+    
     context = {
-        'breakingNews': breakingNews
+        'breakingNews': breakingNews,
+        'topArticle': topArticle,
     }
-
+    
     return render(request, 'pages/index.html', context)
