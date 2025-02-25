c2c826ce48a3e92b5e8ae1dd1a2992a050791357
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-25 11:56:14 +0200

    Add logic to retrieve recent article in home view

--- base/views.py
+++ base/views.py
@@ -5,31 +5,35 @@ from django.shortcuts import render
 
 def home(request):
     breakingNews = Article.objects.all().order_by('-id')[:4]
-    
+
     # Define the time threshold for 24 hours ago
     timeThreshold = timezone.now() - timedelta(hours=24)
-    
+
     # Retrieve the article with the most views in the last 24 hours
     topArticle = Article.objects.filter(created_at__gte=timeThreshold).order_by('-views').first()
-    
+
     # Retrieve 3 trending articles from the last 24 hours, excluding the topArticle to avoid duplication
     if topArticle:
         trendingArticles = Article.objects.filter(created_at__gte=timeThreshold).exclude(id=topArticle.id).order_by('-views')[:3]
     else:
         trendingArticles = Article.objects.filter(created_at__gte=timeThreshold).order_by('-views')[:3]
-    
+
     # Retrieve 2 articles in random order
     randomArticles = Article.objects.all().order_by('?')[:2]
-    
+
     # Retrieve all categories
     categories = Category.objects.all()
-    
+
+    # Retrieve the recent article from the last 24 hours (most recent creation date)
+    recentArticle = Article.objects.filter(created_at__gte=timeThreshold).order_by('-created_at').first()
+
     context = {
         'breakingNews': breakingNews,
         'topArticle': topArticle,
         'trendingArticles': trendingArticles,
         'randomArticles': randomArticles,
         'categories': categories,
+        'recentArticle': recentArticle,
     }
-    
+
     return render(request, 'pages/index.html', context)
