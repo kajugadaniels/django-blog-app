f62499a39fe649b88c8fff82153348b3853d65cd
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-25 17:11:26 +0200

    Fix annotation for most liked article by using reverse accessor 'like' instead of 'like_set'

--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
 from base.models import *
 from datetime import timedelta
 from django.utils import timezone
-from django.shortcuts import render
 from django.db.models import Count
+from django.shortcuts import render
 
 def home(request):
     breakingNews = Article.objects.all().order_by('-id')[:4]
@@ -15,9 +15,12 @@ def home(request):
     
     # Retrieve 3 trending articles from the last 24 hours, excluding the topArticle to avoid duplication
     if topArticle:
-        trendingArticles = Article.objects.filter(created_at__gte=timeThreshold).exclude(id=topArticle.id).order_by('-views')[:3]
+        trendingArticles = Article.objects.filter(created_at__gte=timeThreshold)\
+                                          .exclude(id=topArticle.id)\
+                                          .order_by('-views')[:3]
     else:
-        trendingArticles = Article.objects.filter(created_at__gte=timeThreshold).order_by('-views')[:3]
+        trendingArticles = Article.objects.filter(created_at__gte=timeThreshold)\
+                                          .order_by('-views')[:3]
     
     # Retrieve 2 articles in random order
     randomArticles = Article.objects.all().order_by('?')[:2]
@@ -26,7 +29,9 @@ def home(request):
     categories = Category.objects.all()
     
     # Retrieve the recent article from the last 24 hours (most recent creation date)
-    recentArticle = Article.objects.filter(created_at__gte=timeThreshold).order_by('-created_at').first()
+    recentArticle = Article.objects.filter(created_at__gte=timeThreshold)\
+                                   .order_by('-created_at')\
+                                   .first()
     
     # Retrieve 6 articles, one from each category (for 6 randomly selected categories)
     categoryArticles = []
@@ -41,15 +46,19 @@ def home(request):
     
     # Advanced logic:
     # Determine the category that has the most articles overall.
-    mostPopulatedCategory = Category.objects.annotate(articleCount=Count('article')).order_by('-articleCount').first()
+    mostPopulatedCategory = Category.objects.annotate(articleCount=Count('article'))\
+                                            .order_by('-articleCount')\
+                                            .first()
     # From that category, retrieve one article that is recent (within the last 24 hours) and has the most views.
     if mostPopulatedCategory:
-        advancedArticle = Article.objects.filter(category=mostPopulatedCategory, created_at__gte=timeThreshold).order_by('-views').first()
+        advancedArticle = Article.objects.filter(category=mostPopulatedCategory, created_at__gte=timeThreshold)\
+                                           .order_by('-views')\
+                                           .first()
     else:
         advancedArticle = None
 
-    # New logic: Retrieve 3 related articles to the advancedArticle.
-    # Related articles are those in the same category as advancedArticle, created in the last 24 hours, and not the advancedArticle itself.
+    # Retrieve 3 related articles to the advancedArticle.
+    # Related articles are defined as those in the same category as advancedArticle, created in the last 24 hours, and not the advancedArticle itself.
     if advancedArticle:
         relatedArticles = Article.objects.filter(
             category=advancedArticle.category,
@@ -59,9 +68,9 @@ def home(request):
         relatedArticles = None
 
     # New logic: Retrieve the most liked article in the last 24 hours.
-    # We annotate each article with its like count (using the default reverse relationship "like_set")
+    # Annotate each article with its like count using the reverse accessor 'like'
     mostLikedArticle = Article.objects.filter(created_at__gte=timeThreshold)\
-        .annotate(like_count=Count('like_set'))\
+        .annotate(like_count=Count('like'))\
         .order_by('-like_count')\
         .first()
     
