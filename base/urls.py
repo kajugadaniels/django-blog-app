7bfc429b0581307cce2e9a56b8c439c6a8ceebe9
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-04 14:39:26 +0200

    Add route for showArticle view using article slug

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ app_name = 'base'
 
 urlpatterns = [
     path('', home, name="home"),
+    path('article/<slug:slug>/', showArticle, name='showArticle'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
