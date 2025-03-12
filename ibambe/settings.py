733c4a0feb8f41b210f5faf4be90fae63e4ea8f8
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 15:10:05 +0200

    New changes

--- ibambe/settings.py
+++ ibambe/settings.py
@@ -78,14 +78,21 @@ WSGI_APPLICATION = 'ibambe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DB"),
-        'USER': os.getenv("MYSQL_USER"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-        'HOST': os.getenv("MYSQL_HOST"),
-        'PORT': os.getenv("MYSQL_PORT", "3306"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "irebecor_irebe",
+#         'USER': "irebecor_user",
+#         'PASSWORD': "&IacydbSW(IF",
+#         'HOST': "localhost",
+#         'PORT': "3306",
+#     }
+# }
 
 
 # Password validation
